5d9129d00368c6c10b8a85e02cfa3bc110342074
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-28 22:50:32 +0900

    Create 2.      .c

---
+++ Other exercises/2.      .c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int n;
+    float sum = 0;
+
+    do {
+	printf("Enter a number = ");
+	scanf("%d", &n);
+	if (n < 0)
+		printf("Eror /n");
+	} while (n < 0);
+
+     for (int i = 1; i <= n; i++) {
+		sum += 1.0 / i;
+     }
+     printf("S(%d) = %.2f  \n", n, sum);
+     return 0;
+}
