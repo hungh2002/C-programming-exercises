32d14226a2fd72e1fa9764886b6e34bd6a572626
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-29 18:40:28 +0900

    Create 5.   S(n)= 1 + (1:3) + (1:5) +...+ (1: (2*n+1) )     .c

---
+++ Other exercises/5.   S(n)= 1 + (1:3) + (1:5) +...+ (1: (2*n+1) )     .c
@@ -0,0 +1,26 @@
+//  S(n)= 1 + (1/3) + (1/5) +...+ (1/ (2*n+1) )
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n;
+	float sum = 0;
+
+	do
+	{
+		printf("Enter a number = ");
+		scanf("%d", &n);
+		if (n < 0)
+			printf("Eror /n");
+	} while (n < 0);
+
+	for (int i = 0; i <= n; i++)
+	{
+		sum += 1.0 / (2*i + 1);
+	}
+
+	printf("S(%d) = %.2f  \n", n, sum);
+	return 0;
+}
