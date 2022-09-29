96b95e8f30c5f29750f165ed549db32d98a3ebb3
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-29 22:37:41 +0900

    Create 8.   S(n)= (1:2) + (3:4) + (5:6) +...+  ( (2*n + 1):  (2*n + 2) )    .c

---
+++ Other exercises/8.   S(n)= (1:2) + (3:4) + (5:6) +...+  ( (2*n + 1):  (2*n + 2) )    .c
@@ -0,0 +1,27 @@
+// S(n)= (1/2) + (3/4) + (5/6) +...+  ( (2*n + 1)/ (2*n + 2) )
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n;
+	float sum = 0;
+
+	do
+	{
+		printf("Enter a number = ");
+		scanf("%d", &n);
+		if (n < 0)
+			printf("Eror /n");
+	} while (n < 0);
+
+	for (int i = 0; i <= n; i++)
+	{
+		sum += (float) (2*i + 1) / (2*i + 2);
+	}
+
+	printf("S(%d) = %.2f  \n", n, sum);
+	return 0;
+}
