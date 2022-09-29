73efd9d7e1df7a828f6cf4331042e2ceb39197db
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-29 19:13:27 +0900

    Create 6.    S(n)= (1:2) + (1:6) +...+ (1: (n* (n+1) )     .c

---
+++ Other exercises/6.    S(n)= (1:2) + (1:6) +...+ (1: (n* (n+1) )     .c
@@ -0,0 +1,27 @@
+// S(n)= (1/2) + (1/6) +...+ (1/ (n* (n+1) )
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n;
+	float sum = 0;
+
+	do
+	{
+		printf("Enter a number = ");
+		scanf("%d", &n);
+		if (n < 0)
+			printf("Eror /n");
+	} while (n < 0);
+
+	for (int i = 1; i <= n; i++)
+	{
+		sum += 1.0 / ( i * (i + 1) );
+	}
+
+	printf("S(%d) = %.2f  \n", n, sum);
+	return 0;
+}
