0f21e14b30468808014eeb9dd68b9d5b29e4268b
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-29 22:18:52 +0900

    Create 7.    S(n)= (1:2) + (2:3) + (3:4) +...+ (n: (n +1) )    .c

---
+++ Other exercises/7.    S(n)= (1:2) + (2:3) + (3:4) +...+ (n: (n +1) )    .c
@@ -0,0 +1,27 @@
+// S(n)= (1/2) + (2/3) + (3/4) +...+ (n/ (n + 1) )
+
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n;
+	float sum = 0;
+
+	do
+	{
+		printf("Enter a number = ");
+		scanf("%d", &n);
+		if (n < 0)
+			printf("Eror /n");
+	} while (n < 0);
+
+	for (int i = 1; i <= n; i++)
+	{
+		sum += (float) i / (i + 1);
+	}
+
+	printf("S(%d) = %.2f  \n", n, sum);
+	return 0;
+}
