ea916668091ac07d8340b847a155f363dc05242b
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-29 18:53:57 +0900

    Update 2. S(n) = (1^2) + (2^2) + (3^2) + … + (n^2)   .c

--- Other exercises/2. S(n) = (1^2) + (2^2) + (3^2) + … + (n^2)   .c
+++ Other exercises/2. S(n) = (1^2) + (2^2) + (3^2) + … + (n^2)   .c
@@ -5,8 +5,14 @@
 
 int main () {
     int n, sum=0;
-    printf ("Enter a number = ");
-    scanf ("%d", &n);
+
+    do {
+	printf("Enter a number = ");
+	scanf("%d", &n);
+	if (n < 0)
+		printf("Eror /n");
+	} while (n < 1);
+
     for (int i = 1; i <= n; i++) {
         sum += pow (i,2);
     }
