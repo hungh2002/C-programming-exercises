3d87f08f7249bf1253ed3ec6441bb285ffdf6597
Luu Manh Hung <manhhung.h2002@gmail.com>
2022-09-30 17:21:47 +0900

    Create 9.   S(n)= 1 * 2 * 3 *...* n     .c

---
+++ Other exercises/9.   S(n)= 1 * 2 * 3 *...* n     .c
@@ -0,0 +1,22 @@
+// S(n)= 1 * 2 * 3 *...* n
+
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int n;
+    unsigned int sum= 1;
+
+    do {
+	printf("Enter a number = ");
+	scanf("%d", &n);
+	if (n < 1)
+		printf("Eror /n");
+	} while (n < 1);
+
+    for (int i = 1; i <= n; i++) {
+        sum *= i;
+    }
+    printf ("S(%d) = %d \n", n, sum);
+    return 0;
+}
